db5891f8b90289ac4fe9ff1464d690b530094341
daniaggbro-cloud <daniaggbro@gmail.com>
2026-04-06 14:41:53 +0300

    Add files via upload

--- TumbaHub-main (3)/TumbaHub-main/tumbaHub/gui/main_window.lua
+++ TumbaHub-main (3)/TumbaHub-main/tumbaHub/gui/main_window.lua
@@ -440,7 +440,7 @@ local ToggleButton = Instance.new("ImageButton", MobileGUI)
 ToggleButton.Name = "TumbaMenuIcon"
 ToggleButton.Size = UDim2.new(0, 38, 0, 38) -- Made smaller (from 44 to 38)
 ToggleButton.AnchorPoint = Vector2.new(1, 0) -- Right aligned
-ToggleButton.Position = UDim2.new(1, -10, 0, 15) -- 10px from the right edge
+ToggleButton.Position = UDim2.new(1, -10, 0, 5) -- Moved higher to avoid overlap
 ToggleButton.BackgroundTransparency = 1 -- Fully transparent background for a clean icon
 -- Вставь сюда RAW-ссылку на свою картинку на GitHub!
 local customLogoUrl = "https://raw.githubusercontent.com/daniaggbro-cloud/betatesttumba/main/TumbaHub-main%20(3)/TumbaHub-main/tumbaHub/icon/%D0%97%D0%BD%D1%96%D0%BC%D0%BE%D0%BA%20%D0%B5%D0%BA%D1%80%D0%B0%D0%BD%D0%B0%202026-04-06%20004609.png"
